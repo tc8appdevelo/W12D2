ca8257deffbe9743da158c9bb36c992863bc4747
tc8appdevelo <tc8appdevelo@gmail.com>
2021-12-28 13:07:19 -0600

    coded sessions_controller.rb and users_controller.rb

---
+++ benchbnb/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+    
+    def create
+        user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if user.nil?
+            flash.now[:errors] = ["Invalid credentials!"]
+        elsif !user.activated?
+            flash.now[:errors] = ['activate account first']
+        else
+            login_user!(user)
+            puts "login_user!"
+        end
+    end
+
+    def destroy
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        puts "destroy in session ran"
+    end
+end
---
+++ benchbnb/app/controllers/api/users_controller.rb
@@ -0,0 +1,27 @@
+class Api::UsersController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            @user.activate!
+            login_user!(@user)
+            puts "inside of create and if @user.save login_user!(@user)"
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            puts "inside else in create, @user.save false"
+        end
+    end
+
+    def activate
+        @user = User.find_by(activation_token: params[:activation_token])
+        @user.activate!
+        login_user!(@user)
+        flash[:notice] = 'Successfully logged in'
+        puts "logged in activate ran"
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+end
